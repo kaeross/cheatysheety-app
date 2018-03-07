3c6c49fc791475125546325d7d972c5344306c65
Kate Ross <kaeross@icloud.com>
2018-03-07 16:58:31 +0100

    add cheatsheet data

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -12,4 +12,36 @@
 
 <style>
   /* CSS */
+    #app {
+    font-family: 'Avenir Next', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    width: 100vw;
+  }
+  .sidebar {
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(100, 173, 180, .8) 40%,
+        rgba(85, 141, 151, .9) 100%
+        
+      );
+      background-color: #A9A8A9;
+      height: 100vh;
+      color: #fff;
+  }
+
+
+  h1 {
+    color: #64ADB4;
+  }
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import axios from 'axios';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import router from './router';
 import store from './store';
